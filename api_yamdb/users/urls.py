98e361e6569421e42ed30265ed15eb41f7385b6a
Boris Gurzhy <gurzhy@yandex.ru>
2022-12-15 13:12:17 +0300

    tests-fix

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,14 +1,37 @@
 from django.urls import path, include
-from .views import UserViewSet, token, UsersByAdminViewSet
+from .views import UserViewSet, token, UsersByAdminViewSet, MeViewSet
 from rest_framework import routers
 
 user_create = UserViewSet.as_view({"post": "create"})
+by_username = UsersByAdminViewSet.as_view(
+    {
+        "get": "get_by_username",
+        "patch": "patch_by_username",
+        "delete": "delete_by_username",
+    }
+)
 
+me_username = MeViewSet.as_view(
+    {
+        "get": "get_me",
+        "patch": "patch_me",
+    }
+)
 router = routers.SimpleRouter()
 router.register(r"users", UsersByAdminViewSet)
 
 urlpatterns = [
     path("signup/", user_create, name="user-create"),
     path("token/", token, name="token"),
+    path(
+        "users/me/",
+        me_username,
+        name="me-username",
+    ),
+    path(
+        "users/<str:username>/",
+        by_username,
+        name="by-username",
+    ),
     path("", include(router.urls)),
 ]
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,13 +1,44 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
 from .models import User
 from .serializers import UserSerializer, UsersByAdminSerializer
 from rest_framework import permissions
-from rest_framework.decorators import api_view, permission_classes, action
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.pagination import LimitOffsetPagination
+from django.core.mail import EmailMessage
+from rest_framework import filters
+from .permissions import RoleAdmin
+from django.shortcuts import get_object_or_404
+
+
+class MeViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    """Me."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_me(self, request):
+        """Выдаем Me."""
+        return Response(
+            UsersByAdminSerializer(request.user).data,
+            status=status.HTTP_200_OK,
+        )
+
+    def patch_me(self, request):
+        """Патчим Me."""
+        instance = request.user
+        serializer = UsersByAdminSerializer(
+            instance, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save(role=instance.role)
+            return Response(serializer.validated_data)
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -17,19 +48,76 @@ class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = UserSerializer
     permission_classes = (permissions.AllowAny,)
 
+    def send_confirmation_code(self, request):
+        """После сохранения пользователя отправляем по почте confirmation_code."""
+        email = request.data.get("email")
+        confirmation_code = request.data.get("confirmation_code")
+        username = request.data.get("confirmation_code")
+        email2send = EmailMessage(
+            f"confirmation_code for user {username}",
+            str(confirmation_code),
+            email,
+            [email],
+        )
+        email2send.send()
+
+    def create(self, request, *args, **kwargs):
+        """Самостоятельная регистрация новых пользователей."""
+        username = request.data.get("username")
+        email = request.data.get("email")
+        if username == "me":
+            return Response(
+                {"error": "username=me is depricated"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        user = User.objects.filter(username=username).first()
+        if user:
+            if email != user.email:
+                return Response(
+                    {"error": "incorrect email"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            self.send_confirmation_code(request)
+            return Response(
+                {"email": user.email, "username": user.username},
+                status=status.HTTP_200_OK,
+            )
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        self.send_confirmation_code(request)
+        return Response(
+            serializer.data, status=status.HTTP_200_OK, headers=headers
+        )
+
 
 @api_view(["POST"])
-@permission_classes([AllowAny])
+@permission_classes([permissions.AllowAny])
 def token(request):
     """Токен."""
-    user = get_object_or_404(User, username=request.data.get("username"))
+    username = request.data.get("username")
+    if not username:
+        return Response(
+            ["username is required field."],
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    user = User.objects.filter(username=username).first()
+    if not user:
+        return Response(
+            ["incorrect username."],
+            status=status.HTTP_404_NOT_FOUND,
+        )
     confirmation_code = request.data.get("confirmation_code")
     if not confirmation_code:
-        return Response(["confirmation_code is required."], status=400)
+        return Response(
+            ["confirmation_code is required."],
+            status=status.HTTP_403_FORBIDDEN,
+        )
     if str(confirmation_code) != str(user.confirmation_code):
         return Response(
             ["Incorrect confirmation_code"],
-            status=401,
+            status=status.HTTP_400_BAD_REQUEST,
         )
     refresh = RefreshToken.for_user(user)
     return Response(
@@ -45,13 +133,49 @@ class UsersByAdminViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UsersByAdminSerializer
     pagination_class = LimitOffsetPagination
-    permission_classes = (IsAdminUser,)
+    permission_classes = [RoleAdmin]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("username",)
 
-    # https://stackoverflow.com/questions/30170848/how-can-i-make-a-django-rest-framework-me-call
-    @action(
-        methods=("GET", "PATCH"), detail=False, url_path="me", url_name="me"
-    )
-    def me(self, request, *args, **kwargs):
-        """ПОКА НЕ РАБОТАЕТ МЕТОД PATCH."""
-        self.kwargs.update(pk=request.user.id)
-        return self.retrieve(request, *args, **kwargs)
+    def create(self, request, *args, **kwargs):
+        """Создаем пользователя."""
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+    def get_by_username(self, request, username):
+        """Выдаем пользователя не по pk, а по имени."""
+        user = get_object_or_404(User, username=username)
+        return Response(
+            UsersByAdminSerializer(user).data, status=status.HTTP_200_OK
+        )
+
+    def patch_by_username(self, request, username):
+        """Патчим пользователя не по pk, а по имени."""
+        instance = get_object_or_404(User, username=username)
+        serializer = UsersByAdminSerializer(
+            instance, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data)
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def delete_by_username(self, request, username):
+        """Удаляем пользователя не по pk, а по имени."""
+        user = get_object_or_404(User, username=username)
+        user.delete()
+        return Response(
+            {"result": "user is deleted"}, status=status.HTTP_204_NO_CONTENT
+        )
