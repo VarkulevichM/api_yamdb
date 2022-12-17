97c18acc5d7b99d38dd08469399bdee233d0368a
Михаил Варкулевич <workmailkra@gmail.com>
2022-12-17 20:51:56 +0700

    Первый вариант нашего API

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -14,15 +14,13 @@ class AdminOrReadOnly(permissions.BasePermission):
 class AdminModeratorAuthorOrReadOnly(permissions.BasePermission):
     """Разрешения для CommentViewSet,ReviewViewSet"""
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role == "admin"
-            or request.user.role == "moderator"
-            or obj.author == request.user
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.role == "admin"
+                or request.user.role == "moderator"
+                or obj.author == request.user
+                )
 
     def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+                )
